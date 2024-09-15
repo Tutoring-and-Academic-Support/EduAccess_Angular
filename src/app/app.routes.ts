0bc447ebe55f217f24567b2c1a88fd8876914ee4
johanVeneros <fabriveneros@hotmail.com>
2024-09-14 19:51:26 -0500

    SCRUM-33

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PaginaInicioComponent } from './pagina-inicio/pagina-inicio.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: PaginaInicioComponent },// Ruta para el componente principal de la página de inicio
+    { path: 'dashboard', component: DashboardComponent }
+  ];
